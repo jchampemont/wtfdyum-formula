2a21fb0a31ef8a9e5086bec2d4a1563f1c363d21
Jean Champémont <git@jeanchampemont.com>
2018-04-02 15:12:28 +0200

    add adminId pillar

--- wtfdyum/files/startup.sh
+++ wtfdyum/files/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar wtfdyum-{{ wtfdyum.version }}.jar --wtfdyum.twitter.appId={{ wtfdyum.appId }} --wtfdyum.twitter.appSecret={{ wtfdyum.appSecret }} --wtfdyum.server-base-url={{ wtfdyum.url }} --wtfdyum.tracking.code="{{ wtfdyum.trackingCode }}"
+java -jar wtfdyum-{{ wtfdyum.version }}.jar --wtfdyum.twitter.appId={{ wtfdyum.appId }} --wtfdyum.twitter.appSecret={{ wtfdyum.appSecret }} --wtfdyum.server-base-url={{ wtfdyum.url }} --wtfdyum.admin.twitterId={{ wtfdyum.adminId }} --wtfdyum.tracking.code="{{ wtfdyum.trackingCode }}"
